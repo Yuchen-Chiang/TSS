222e10a5f2f6febbe01ba21ed813f55b7b573c1a
Yuchen Chiang <yuchenchiang0330@gmail.com>
2018-10-22 03:10:24 +0800

    将应用部署到阿里云所做修改

--- src/main/java/com/silver/tss/TssApplication.java
+++ src/main/java/com/silver/tss/TssApplication.java
@@ -1,8 +1,6 @@
 package com.silver.tss;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -15,8 +13,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.silver.tss.dao")
 public class TssApplication {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(TssApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(TssApplication.class, args);
